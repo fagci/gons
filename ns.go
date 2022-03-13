9fd0cfc0ffe41418434357e880e47c0cff900f56
Mikhail Yudin <fagci.nsk@gmail.com>
2022-03-13 21:14:01 +0700

    fix: rm concurrency in inf loop =)

--- ns.go
+++ ns.go
@@ -3,36 +3,36 @@ package main
 import (
 	"fmt"
 	"go_ns/src/gen"
+	"net"
+	"runtime"
 	"time"
-    "net"
-    "runtime"
 )
 
 const PORT = "80"
 
 func check(ip string, port string) {
-    addr := ip + ":" + port
+	addr := ip + ":" + port
 
-    d := net.Dialer{Timeout: time.Second}
-    c, e := d.Dial("tcp", addr)
+	d := net.Dialer{Timeout: time.Second}
+	c, e := d.Dial("tcp", addr)
 
-    if e == nil {
-        c.Close()
-        fmt.Println(addr)
-    }
+	if e == nil {
+		c.Close()
+		fmt.Println(addr)
+	}
 }
 
 func worker() {
 	gen_ip := gen.WanIpGenerator()
-    for {
-        go check(gen_ip().String(), PORT)
-    }
+	for {
+		check(gen_ip().String(), PORT)
+	}
 }
 
 func main() {
-    for i := 0; i < 1024; i++ {
-        go worker()
-    }
+	for i := 0; i < 1024; i++ {
+		go worker()
+	}
 
-    runtime.Goexit()
+	runtime.Goexit()
 }
