763604e6e75386525464fc811087d582d095c394
Mikhail Yudin <fagci.nsk@gmail.com>
2022-03-24 00:39:21 +0700

    refactor, stabilize

--- src/protocol/rtsp.go
+++ src/protocol/rtsp.go
@@ -20,33 +20,41 @@ type RTSP struct {
 	ch      chan string
 }
 
-const RW_TIMEOUT = time.Second * 5
+const RW_TIMEOUT = time.Second * 10
 
 const _RTSP_TPL = "%s %s RTSP/1.0\r\n" +
+	"Accept: application/sdp\r\n" +
 	"CSeq: %d\r\n" +
-	"User-Agent: LibVLC/3.0.0\r\n" +
-	"Accept: application/sdp\r\n\r\n"
+	"User-Agent: Lavf59.16.100\r\n\r\n"
 
 func (r *RTSP) Request(req string) (int, error) {
-	r.cseq++
+
 	if e := r.conn.SetDeadline(time.Now().Add(RW_TIMEOUT)); e != nil {
 		return 0, e
 	}
+
 	if _, e := r.conn.Write([]byte(req)); e != nil {
 		return 0, e
 	}
 
-	m := make([]byte, 1024)
-	n, e := r.conn.Read(m)
+	responseBytes := make([]byte, 1024)
+	n, e := r.conn.Read(responseBytes)
 	if e != nil {
 		return 0, e
 	}
 
-	resp := string(m[:n])
+	response := string(responseBytes[:n])
 
-	f := strings.Fields(resp)
+	f := strings.Fields(response)
 	if len(f) > 2 && strings.HasPrefix(f[0], "RTSP") {
-		return strconv.Atoi(f[1])
+		code, e := strconv.Atoi(f[1])
+		/* if e != nil || (code != 200 && code != 401 && code != 404) {
+			fmt.Println("Err:", e)
+			fmt.Println("Code:", code)
+			fmt.Println(req)
+			fmt.Println("----")
+		} */
+		return code, e
 	}
 
 	return 0, errors.New("Bad response")
@@ -57,8 +65,11 @@ func (r *RTSP) Query(path string) string {
 
 	if path == "*" {
 		method = "OPTIONS"
+	} else {
+		path = fmt.Sprintf("rtsp://%s:%d%s", r.Host, r.Port, path)
 	}
 
+	r.cseq++
 	return fmt.Sprintf(_RTSP_TPL, method, path, r.cseq)
 }
 
@@ -70,9 +81,11 @@ func (r *RTSP) Check() <-chan string {
 }
 
 func (r *RTSP) check() {
-	defer close(r.ch)
-	address := fmt.Sprintf("%s:%d", r.Host, r.Port)
 	var err error
+
+	defer close(r.ch)
+
+	address := fmt.Sprintf("%s:%d", r.Host, r.Port)
 
 	if r.conn, err = net.DialTimeout("tcp", address, r.timeout); err != nil {
 		return
