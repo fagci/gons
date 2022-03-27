16fd7e1fd35dffbe41f9e3733ebfd6f87ba9aed9
Mikhail Yudin <fagci.nsk@gmail.com>
2022-03-28 02:29:31 +0700

    feat: async port scan for each host

--- src/services/portscan.go
+++ src/services/portscan.go
@@ -3,6 +3,7 @@ package services
 import (
 	"gons/src/models"
 	"net"
+	"sync"
 	"time"
 )
 
@@ -15,19 +16,25 @@ func NewPortscanService(ports []int, timeout time.Duration) *PortscanService {
 	return &PortscanService{Ports: ports, timeout: timeout}
 }
 
+func (s *PortscanService) ScanAddr(addr net.TCPAddr, ch *chan models.HostResult, wg *sync.WaitGroup) {
+    defer wg.Done()
+	if conn, err := net.DialTimeout("tcp", addr.String(), s.timeout); err == nil {
+		conn.Close()
+		*ch <- models.HostResult{Addr: &addr}
+	}
+}
+
 func (s *PortscanService) Check(ip net.IP) <-chan models.HostResult {
+    var wg sync.WaitGroup
 	ch := make(chan models.HostResult)
 	go func() {
 		defer close(ch)
 		for _, port := range s.Ports {
 			addr := net.TCPAddr{IP: ip, Port: port}
-			if conn, err := net.DialTimeout("tcp", addr.String(), s.timeout); err == nil {
-				conn.Close()
-				ch <- models.HostResult{
-					Addr: &addr,
-				}
-			}
+            wg.Add(1)
+            go s.ScanAddr(addr, &ch, &wg)
 		}
+        wg.Wait()
 	}()
 	return ch
 }
