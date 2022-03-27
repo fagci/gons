a39f0abe1776ef73b0e2e640557c0946469e1af6
Mikhail Yudin <fagci.nsk@gmail.com>
2022-03-27 23:00:33 +0700

    test: improvement IPGenerator test

--- src/generators/ip_test.go
+++ src/generators/ip_test.go
@@ -6,9 +6,14 @@ import (
 )
 
 func TestIPGen(t *testing.T) {
-    fmt.Println("Start")
-    ipGen := NewIPGenerator(1, 5)
-    for ip := range ipGen.GenerateWAN() {
-        fmt.Println(ip.String())
-    }
+	const N = 5
+	var i int
+	ipGen := NewIPGenerator(1, 5)
+	for ip := range ipGen.GenerateWAN() {
+		fmt.Println(ip.String())
+		i++
+	}
+	if i != N {
+		t.Error("Wrong IP count")
+	}
 }
