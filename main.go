bf961470c6a88c8a47143a8c2446016a53eee908
Mikhail Yudin <fagci.nsk@gmail.com>
2022-03-28 12:16:39 +0700

    refactor: better options

--- main.go
+++ main.go
@@ -13,29 +13,49 @@ import (
 	"time"
 )
 
-var generateIps = flag.Int64("n", -1, "generate N random WAN IPs")
-var scanWorkers = flag.Int("w", 1024, "workers count")
-var connTimeout = flag.Duration("t", 700*time.Millisecond, "scan connect timeout")
-var scanPorts = flag.String("p", "", "scan ports on every rarget")
+var randomIPsCount int64
+var scanWorkers, resultCallbackConcurrency int
+var connTimeout, resultCallbackTimeout time.Duration
 
-var resultCallback = flag.String("cb", "", "callback to run as shell command. Use {result} as template")
-var resultCallbackTimeout = flag.Duration("cbt", 30*time.Second, "callback timeout")
-var resultCallbackConcurrency = flag.Int("cbmc", 30, "callback max concurrency")
-var resultCallbackE = flag.Bool("cbde", false, "disable callback errors")
-var resultCallbackW = flag.Bool("cbdw", false, "disable callback warnings")
-var resultCallbackI = flag.Bool("cbdi", false, "disable callback info")
+var resultCallback string
+var resultCallbackI, resultCallbackE, resultCallbackW bool
 
-var scanRtsp = flag.Bool("rtsp", false, "check rtsp")
-var rtspFuzzDict = flag.String("rtspd", "./data/rtsp-paths.txt", "RTSP dictionary to fuzz")
+var scanPorts string
 
-var cpuprofile = flag.String("profcpu", "", "profile cpu")
-var memprofile = flag.String("profmem", "", "profile mem")
+var scanRtsp bool
+var rtspFuzzDict string
+
+var cpuprofile, memprofile string
+
+func init() {
+	flag.Int64Var(&randomIPsCount, "n", -1, "generate N random WAN IPs")
+	flag.IntVar(&scanWorkers, "w", 1024, "workers count")
+	flag.IntVar(&scanWorkers, "workers", 1024, "workers count")
+	flag.DurationVar(&connTimeout, "t", 700*time.Millisecond, "scan connect timeout")
+	flag.DurationVar(&connTimeout, "timeout", 700*time.Millisecond, "scan connect timeout")
+
+	flag.StringVar(&resultCallback, "cb", "", "callback to run as shell command. Use {result} as template")
+	flag.DurationVar(&resultCallbackTimeout, "cbt", 30*time.Second, "callback timeout")
+	flag.IntVar(&resultCallbackConcurrency, "cbmc", 30, "callback max concurrency")
+	flag.BoolVar(&resultCallbackI, "cbdi", false, "disable callback infos")
+	flag.BoolVar(&resultCallbackW, "cbdw", false, "disable callback warnings")
+	flag.BoolVar(&resultCallbackE, "cbde", false, "disable callback errors")
+
+	flag.StringVar(&scanPorts, "p", "", "scan ports on every rarget")
+	flag.StringVar(&scanPorts, "ports", "", "scan ports on every rarget")
+
+	flag.BoolVar(&scanRtsp, "rtsp", false, "check rtsp")
+	flag.StringVar(&rtspFuzzDict, "rtspd", "./data/rtsp-paths.txt", "RTSP dictionary to fuzz")
+
+	flag.StringVar(&cpuprofile, "profcpu", "", "profile cpu")
+	flag.StringVar(&memprofile, "profmem", "", "profile mem")
+}
 
 func main() {
 	flag.Parse()
 
-	if *cpuprofile != "" {
-		f, err := os.Create(*cpuprofile)
+	if cpuprofile != "" {
+		f, err := os.Create(cpuprofile)
 		if err != nil {
 			utils.EPrintln("[E]", err)
 			os.Exit(1)
@@ -44,8 +64,8 @@ func main() {
 		defer pprof.StopCPUProfile()
 	}
 
-	if *memprofile != "" {
-		f, err := os.Create(*memprofile)
+	if memprofile != "" {
+		f, err := os.Create(memprofile)
 		if err != nil {
 			utils.EPrintln("[E]", err)
 			os.Exit(1)
@@ -54,34 +74,34 @@ func main() {
 		defer pprof.WriteHeapProfile(f)
 	}
 
-	ipGenerator := generators.NewIPGenerator(128, *generateIps)
-	processor := services.NewProcessor(ipGenerator, *scanWorkers)
+	ipGenerator := generators.NewIPGenerator(128, randomIPsCount)
+	processor := services.NewProcessor(ipGenerator, scanWorkers)
 
 	var cbFlags utils.Flags
-	if !*resultCallbackE {
+	if !resultCallbackE {
 		cbFlags = cbFlags.Set(utils.ERR)
 	}
-	if !*resultCallbackW {
+	if !resultCallbackW {
 		cbFlags = cbFlags.Set(utils.WARN)
 	}
-	if !*resultCallbackI {
+	if !resultCallbackI {
 		cbFlags = cbFlags.Set(utils.INFO)
 	}
 
-	if *scanRtsp {
+	if scanRtsp {
 		os.Stderr.WriteString(fmt.Sprintln("[i] Using rtsp"))
-		paths, err := loaders.FileToArray(*rtspFuzzDict)
+		paths, err := loaders.FileToArray(rtspFuzzDict)
 		if err != nil {
 			utils.EPrintln("[E]", err)
 			os.Exit(1)
 		}
-		rtspService := services.NewRTSPService(554, paths, *connTimeout)
+		rtspService := services.NewRTSPService(554, paths, connTimeout)
 		processor.AddService(rtspService)
 	}
 
-	if *scanPorts != "" {
-		ports := utils.ParseRange(*scanPorts)
-		processor.AddService(services.NewPortscanService(ports, *connTimeout))
+	if scanPorts != "" {
+		ports := utils.ParseRange(scanPorts)
+		processor.AddService(services.NewPortscanService(ports, connTimeout))
 	}
 
 	if len(processor.Services()) == 0 {
@@ -89,15 +109,15 @@ func main() {
 	}
 
 	wg := new(sync.WaitGroup)
-	guard := make(chan struct{}, *resultCallbackConcurrency)
+	guard := make(chan struct{}, resultCallbackConcurrency)
 
 	for result := range processor.Process() {
-		if *resultCallback != "" {
+		if resultCallback != "" {
 			wg.Add(1)
 			guard <- struct{}{}
-			cmd := result.ReplaceVars(*resultCallback)
+			cmd := result.ReplaceVars(resultCallback)
 			go func() {
-				utils.RunCommand(cmd, wg, *resultCallbackTimeout, cbFlags)
+				utils.RunCommand(cmd, wg, resultCallbackTimeout, cbFlags)
 				<-guard
 			}()
 		} else {
